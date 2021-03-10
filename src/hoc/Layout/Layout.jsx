e2fe6e09d56476973d284adc953228d6eee2aebe
Nazar <nazarroshchuk@gmail.com>
2021-03-10 21:19:19 +0200

    improved performance and changing folder structure

--- src/hoc/Layout/Layout.jsx
+++ src/hoc/Layout/Layout.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import { Auxx } from '../../hoc/Auxx';
+import { Auxx } from '../Auxx/Auxx';
 import classes from './Layout.module.css'
-import {Toolbar} from "../Navigation/Toolbar/Toolbar";
-import {SideDrawer} from "../Navigation/SideDrawer/SideDrawer";
+import {Toolbar} from "../../components/Navigation/Toolbar/Toolbar";
+import {SideDrawer} from "../../components/Navigation/SideDrawer/SideDrawer";
 
 export class Layout extends Component  {
     state = {
